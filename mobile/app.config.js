dd282ee87b763c0025888ce67357fadd2e08c524
prajwaldhital853-ux <prajwaldhital851@gmail.com>
2026-08-14 16:30:19 +0545

    Bump Android to 3.4.37 for shared account cap fix release.

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     slug: 'nepseghar',
     owner: 'prajwal851s-team',
     scheme: 'nepseghar',
-    version: '3.4.36',
+    version: '3.4.37',
     orientation: 'portrait',
     icon: './assets/nepse-ghar-logo.png',
     userInterfaceStyle: 'dark',
@@ -25,7 +25,7 @@ module.exports = {
     },
     android: {
       package: 'com.nepse.ghar',
-      versionCode: 82,
+      versionCode: 83,
       adaptiveIcon: {
         backgroundColor: '#FFFFFF',
         foregroundImage: './assets/nepse-ghar-logo.png',
