142d2e8345a255de287ed90a89f515a4f165dafa
prajwaldhital853-ux <prajwaldhital851@gmail.com>
2026-08-26 19:37:22 +0545

    Trigger Vercel redeploy

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     slug: 'nepseghar',
     owner: 'prajwal851s-team',
     scheme: 'nepseghar',
-    version: '3.4.45',
+    version: '3.4.46',
     orientation: 'portrait',
     icon: './assets/nepse-ghar-logo.png',
     userInterfaceStyle: 'dark',
@@ -25,7 +25,7 @@ module.exports = {
     },
     android: {
       package: 'com.nepse.ghar',
-      versionCode: 91,
+      versionCode: 92,
       adaptiveIcon: {
         backgroundColor: '#FFFFFF',
         foregroundImage: './assets/nepse-ghar-logo.png',
